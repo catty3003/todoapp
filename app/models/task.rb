2554598983b311036fdb843e036dff4f0a22f83f
catty3003 <catty_chan16@yahoo.com>
2013-12-10 13:39:15 +0100

    distance_from_now_in_days Methode in Task eingefügt

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,9 @@ class Task < ActiveRecord::Base
 	def is_delayed?
  		self.deadline < Date.today
 	end
+
+	def distance_from_now_in_days
+  (self.deadline - Date.today).to_i     
+	end
+
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-	
+
 test "is delayed" do
  task = Task.new
  task.deadline = Date.today - 10.days
@@ -19,5 +19,23 @@ test "is not delayed if deadline is today" do
  task.deadline = Date.today
  assert !task.is_delayed?
 end
+
+test "destance in days from today" do
+  task = Task.new
+  task.deadline = Date.today
+  assert_equal 0, task.distance_from_now_in_days
+end
+
+test "destance in days from yesterday" do
+  task = Task.new
+  task.deadline = Date.today - 1
+  assert_equal -1, task.distance_from_now_in_days
+end
+
+test "destance in days from tomorrow" do
+  task = Task.new
+  task.deadline = Date.today + 1
+  assert_equal 1, task.distance_from_now_in_days
+end 
 
 end
