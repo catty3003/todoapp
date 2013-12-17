a978bea79d2946d46e2f8ea77182647aa99c5d70
catty3003 <catty_chan16@yahoo.com>
2013-12-17 17:28:30 +0100

    Autorisiserung alles auser Index

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -4,6 +4,7 @@ class TasksControllerTest < ActionController::TestCase
   include Devise::TestHelpers
   setup do
     @task = tasks(:one)
+    @user = users(:one)
   end
 
   test "should get index" do
@@ -14,11 +15,13 @@ class TasksControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    sign_in @user
     get :new
     assert_response :success
   end
 
   test "should create task" do
+    sign_in @user
     assert_difference('Task.count') do
       post :create, task: { deadline: @task.deadline, done: @task.done, duration: @task.duration, name: @task.name }
     end
@@ -29,20 +32,28 @@ class TasksControllerTest < ActionController::TestCase
   
 
   test "should get edit" do
+    sign_in @user
     get :edit, id: @task
     assert_response :success
   end
 
   test "should update task" do
+    sign_in @user
     patch :update, id: @task, task: { deadline: @task.deadline, done: @task.done, duration: @task.duration, name: @task.name }
     assert_redirected_to tasks_url
   end
 
   test "should destroy task" do
+    sign_in @user
     assert_difference('Task.count', -1) do
       delete :destroy, id: @task
     end
 
     assert_redirected_to tasks_path
   end
+
+  test "should not get edit if not logged in" do
+  get :edit, id: @task
+  assert_response :redirect
+end
 end
