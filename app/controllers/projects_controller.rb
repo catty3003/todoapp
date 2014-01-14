57554e593bbc6f7d794b0872773d0cc1c4255dbd
catty3003 <catty_chan16@yahoo.com>
2014-01-14 23:36:06 +0100

    Create Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,5 +8,17 @@ class ProjectsController < ApplicationController
 	def new
 	  @project = Project.new
 	end
-	
+
+	def create
+ 	 	@project = Project.new(project_params)
+  	@project.save
+	end
+
+	private
+    def project_params
+      params.require(:project).permit(:name)
+    end 
+
+
 end
+
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -20,4 +20,12 @@ end
   assert_response :success
 end
 
+test "should create project" do
+  sign_in @user
+  assert_difference('Project.count') do
+    xhr :post, :create, project: { name: "Project Name" }
+  end
+  assert_response :success
+end
+
 end
